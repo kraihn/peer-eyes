04f46f403aac9fb3f27d70697e9550e45c5ff2f7
Jared Dickson <kraihn@gmail.com>
2012-02-20 09:34:33 -0500

    Add functionality to initial Viewer.

--- PeerEyesLibrary/Network/Viewer.cs
+++ PeerEyesLibrary/Network/Viewer.cs
@@ -19,14 +19,76 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace PeerEyesLibrary.Network
 {
     public class Viewer
     {
+        private Thread view;
+        public Queue<Image> screenshots;
+
         public Viewer()
         {
+            screenshots = new Queue<Image>();
+            StartViewing();
+        }
 
+        public void StartViewing(){
+            if (view == null || !view.IsAlive)
+            {
+                view = new Thread(new ThreadStart(RunViewer));
+                view.Start();
+            }
+        }
+
+        public void StopViewing()
+        {
+            view.Abort();
+        }
+
+        public void RestartViewing()
+        {
+            StopViewing();
+            StartViewing();
+        }
+
+        public void RunViewer()
+        {
+            //ViewForm form = new ViewForm();
+            //form.Show();
+            while (true)
+            {
+                try
+                {
+                    UdpClient recv = new UdpClient(Info.screencastPort);
+                    IPEndPoint remote = new IPEndPoint(IPAddress.Any, Info.screencastPort);
+
+                    Console.WriteLine("Receiving img...");
+                    byte[] imgData = recv.Receive(ref remote);
+                    Console.WriteLine("Received img");
+
+                    recv.Close();
+                    
+                    MemoryStream ms = new MemoryStream(imgData);
+                    Image img = Image.FromStream(ms);
+                    //DateTime t = DateTime.Now;
+                    //img.Save("C:\\pe-viewer " + t.Hour + " " + t.Minute + " " + t.Second + ".jpg", ImageFormat.Jpeg);
+                    //form.BackgroundImage = img;
+                    //form.Update();
+                    screenshots.Enqueue(img);
+                    
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
         }
     }
 }
